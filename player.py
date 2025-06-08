b4000181d2c96a99e5dcbeada9b482da98ca77eb
thmastin <thmastin@gmail.com>
2025-06-08 01:23:55 -0400

    Add player type

--- player.py
+++ player.py
@@ -6,15 +6,19 @@ class Role(Enum):
     MAFIA = "mafia"
     TOWN = "townsfolk"
 
+class PlayerType(Enum):
+    HUMAN = 0
+    AI = 1
+
 class Player:
-    def __init__(self, name, role, alive=True):
+    def __init__(self, name, role, alive=True, type=PlayerType.AI):
         self.name = name
         self.role = role
         self.alive = alive
+        self.type = type
     
     def assign_role(self):
         if random.random() < 0.5:
             self.role = Role.MAFIA
-            print(self.role)
         else:
             self.role = Role.TOWN
