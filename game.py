6d2cd1eaa36da199af63b18e1e12da3d006f5f6f
thmastin <thmastin@gmail.com>
2025-06-10 09:40:49 -0400

    Fix game loop to check win condtions more often

--- game.py
+++ game.py
@@ -27,50 +27,73 @@ def game_setup():
         players_alive.append(new_player)
 
     #Debug: Print roles to verify
+    print(f"Total Mafia: {num_mafia} Total Townsfolk = {number_of_players - num_mafia}")
     for player in players:
         print(f"{player.name}: {player.role.value}")
-        print(f"Total Mafia: {num_mafia} Total Townsfolk = {number_of_players - num_mafia}")
     
 def game_cycle():
-    mafia = 0
-    townsfolk = 0
     game_phase = "night"
     night_count = 1
     day_count = 1
 
-    # Set the total number of Mafia and Townsfolk
-    for i in range(len(players)):
-        if players[i].role == Role.MAFIA:
-            mafia += 1
-        else:
-            townsfolk += 1
-    
-    while win_check(mafia, townsfolk) is False:
+    while True:
+        update_players_alive(players)
+        mafia, townsfolk = count_roles(players_alive)
+
+        if win_check(mafia, townsfolk):
+            print("Game Over")
+            print(f"{determine_winner(mafia)} have won the game!")
+            break
+
         if game_phase == "night":
             print(f"Night {night_count}:")
             print(f"Players Alive:")
             output_players_alive(players_alive)
             town_killed = night_vote(players_alive)
             print(f"{town_killed.name} was killed last night")
+            town_killed.alive = False
             players_alive.remove(town_killed)
+            townsfolk -= 1
             night_count += 1
-            game_phase = "day"    
+            game_phase = "day"
+
+            update_players_alive(players)
+            mafia, townsfolk = count_roles(players_alive)
+
+            if win_check(mafia, townsfolk):
+                print("Game Over")
+                print(f"{determine_winner(mafia)} have won the game!")
+                break
+
         else: 
             print(f"Day {day_count}:")
             print(f"Players Alive:")
             output_players_alive(players_alive)
             game_phase = "night"
             day_vote(players_alive)
-            day_count += 1                    
-    print("Game Over")
-    print(f"{determine_winner(mafia)} have won the game!")
-    return
+            day_killed = random.choice(players_alive)
+            day_killed.alive = False
+            players_alive.remove(day_killed)
+            print(f"{day_killed.name} was killed, they were {day_killed.role.value}")
+            if day_killed.role is Role.MAFIA:
+                mafia -= 1
+            else:
+                townsfolk -= 1
+            day_count += 1
+
+            update_players_alive(players)
+            mafia, townsfolk = count_roles(players_alive)
+
+            if win_check(mafia, townsfolk):
+                print("Game Over")
+                print(f"{determine_winner(mafia)} have won the game!")
+                break
 
 def win_check(mafia, townsfolk):
-    if townsfolk > 0 and mafia < len(players_alive):
-        return False
-    else:
+    if mafia == 0 or mafia >= townsfolk:
         return True
+    else:
+        return False
 
 def determine_winner(mafia):
     if mafia == 0:
@@ -80,4 +103,22 @@ def determine_winner(mafia):
     
 def output_players_alive(players_alive):
     for player in players_alive:
-        print({player.name})
+        print(player.name)
+
+def update_players_alive(players):
+    players_alive.clear()
+    for player in players:
+        if player.alive is True:
+            players_alive.append(player)
+
+def count_roles(players_alive):
+    mafia = 0
+    townsfolk = 0
+
+    for player in players_alive:
+        if player.role is Role.MAFIA:
+            mafia += 1
+        else:
+            townsfolk += 1
+    
+    return mafia, townsfolk
