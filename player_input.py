47bf41049b0147d7adf721255d4bca2f336922d0
thmastin <thmastin@gmail.com>
2025-06-16 16:23:18 -0400

    Start adding human player voting

--- player_input.py
+++ player_input.py
@@ -1,2 +1,9 @@
+from player import Player
+
 def pause_game():
     input("Press Enter to continue...")
+
+def player_vote(human):
+    if human.role is Role.MAFIA:
+        maifa_kill = input("Type in 'Kill' followed by the plaher name to kill and pres Enter")
+        
