ac6aa158a561fa7d5b83a7690e8870ce46971067
thmastin <thmastin@gmail.com>
2025-06-17 23:43:14 -0400

    Start working on getting the player vote for daytime

--- player_input.py
+++ player_input.py
@@ -4,7 +4,7 @@ def pause_game():
     input("Press Enter to continue...")
 
 def player_vote_mafia():
-    mafia_kill = input("Type in 'Kill' followed by the plaher name to kill and press Enter: ")
+    mafia_kill = input("Type in 'Kill' followed by the player name to kill and press Enter: ")
     split_kill = mafia_kill.split()
     print(split_kill)
     if split_kill[0].lower() != "kill":
@@ -16,4 +16,14 @@ def player_vote_mafia():
     
 def player_discuss():
     return input("Type in a player's name to accuse them of being Mafia and press Enter: ")
+
+def player_vote_town():
+    player_vote = input("Type in ;Vote; followed by the player name to vote for a player to kill and then press Enter: ").lower().split()
+    if player_vote[0] != "vote":
+        print("Invalid input, you must start with the command 'vote'. Try again!")
+        player_vote_town()
+    else:
+        print(f"Player votes to kill {player_vote[1]}")
+        return player_vote
+    
         
